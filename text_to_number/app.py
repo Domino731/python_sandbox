88475274ba83ca638afce299f7b6aa6282188bb3
Domino731 <d.orzechowski18@gmail.com>
2023-02-19 21:11:45 +0100

    text_to_number - tests

--- text_to_number/app.py
+++ text_to_number/app.py
@@ -4,11 +4,22 @@ import string
 # DATE: 19.02.2023
 # Description: create an app that will receive string and print index of each string letter
 
-text = input('What text do you want to convert to numbers')
+def letters_indexes(text):
+    payload = ''
 
-payload = ''
+    for letter in text:
+        try:
+            payload += str(string.ascii_lowercase.index(letter.lower()))
+        except:
+            payload += letter
 
-for letter in text:
-    payload += str(string.ascii_lowercase.index(letter.lower()))
+    return payload
+
+
+input_payload = input('What text do you want to convert to numbers')
+payload = letters_indexes(input_payload)
 
 print('Output: ', payload)
+
+assert letters_indexes('ABCD') == '0123'
+assert letters_indexes('My name is John') == '1224 130124 818 914713'
